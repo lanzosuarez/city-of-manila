e800e4c4ef235fc13141e87302e5d9348e9226b2
Pons Suarez <pons@chatbot.ph>
2020-04-06 21:20:07 +0800

    mobile nav

---
+++ src/components/MobileHeader.js
@@ -0,0 +1,53 @@
+import React, { useContext } from 'react';
+import styled from '@emotion/styled';
+import { NavigationContext } from '../context/NavigationProvider';
+
+const Container = styled.div`
+  display: none;
+  height: 100px;
+  align-items: center;
+  padding: 0px 50px;
+  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.4);
+  z-index: 3;
+  position: relative;
+
+  @media (max-width: 1024px) {
+    display: flex;
+  }
+
+  @media (max-width: 575.98px) {
+    padding: 0px 20px;
+  }
+`;
+
+const Logo = styled.div`
+  height: 100%;
+  width: 200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: black;
+  font-size: 1.5rem;
+  align-items: center;
+  flex: 1;
+  font-weight: bold;
+`;
+
+const MobileHeader = () => {
+  const { toggleNav } = useContext(NavigationContext);
+  const showNav = () => toggleNav(true);
+  return (
+    <Container>
+      <ion-icon
+        onClick={showNav}
+        style={{ cursor: 'pointer' }}
+        name="menu-outline"
+        size="large"
+      ></ion-icon>
+      <Logo>logo .</Logo>
+      <div />
+    </Container>
+  );
+};
+
+export default MobileHeader;
