9b8fb0bbe2fb0cfad2ecc1479d305aa22ac3359a
Pons Suarez <pons@chatbot.ph>
2020-04-07 22:30:41 +0800

    covid section 1 with accordion

--- src/pages/covid.js
+++ src/pages/covid.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Layout from '../components/Layout';
 import SEO from '../components/seo';
 import NavigationProvider from '../context/NavigationProvider';
+import CovidSection1 from '../components/CovidSection1';
 
 const CovidPage = () => {
   return (
     <NavigationProvider>
       <Layout>
         <SEO title="COVID-19" />
-        <h1>COVID-19</h1>
+        <CovidSection1 />
       </Layout>
     </NavigationProvider>
   );
