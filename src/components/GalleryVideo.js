27a7ef96ef111ec4e8db0dda1bb480099e6bf5e7
Pons Suarez <pons@chatbot.ph>
2020-04-29 15:14:30 +0800

    fhcnages

--- src/components/GalleryVideo.js
+++ src/components/GalleryVideo.js
@@ -2,19 +2,19 @@ import React from 'react';
 import ReactPlayer from 'react-player';
 import '../styles/index.css';
 
-const GalleryVideo = (props) => {
-    return (
-      <div className='player-wrapper'>
-        <ReactPlayer
-          className='react-player'
-          url= {props.url}
-          width='100%'
-          height='100%'
-          controls = {true}
-          playing = {false}
-        />
-      </div>
-    )
+const GalleryVideo = props => {
+  return (
+    <div className="player-wrapper">
+      <ReactPlayer
+        className="react-player"
+        url={props.url}
+        width="100%"
+        height="100%"
+        controls={true}
+        playing={false}
+      />
+    </div>
+  );
 };
 
 export default GalleryVideo;
